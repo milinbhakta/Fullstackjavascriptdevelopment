b485c439c9a9708771d1a6ebc2a80e4250ec3dfb
Milind <desire2dosomething@gmail.com>
2020-02-24 23:21:37 -0500

    using sass for contests display

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-import config from './config';
-import apiRouter from './api';
+import nodeSassMiddleware from "node-sass-middleware";
+import path from "path";
+import config from "./config";
+import apiRouter from "./api";
+import express from "express";
 
-import express from 'express';
 const server = express();
 
-server.set('view engine', 'ejs');
+server.use(nodeSassMiddleware({
+  src:path.join(__dirname,'sass'),
+  dest:path.join(__dirname,'public')
+}));
 
-server.get('/', (req, res) => {
-  res.render('index', {
-    content: '...'
+server.set("view engine", "ejs");
+
+server.get("/", (req, res) => {
+  res.render("index", {
+    content: "..."
   });
 });
 
-server.use('/api', apiRouter);
-server.use(express.static('public'));
+server.use("/api", apiRouter);
+server.use(express.static("public"));
 
 server.listen(config.port, () => {
-  console.info('Express listening on port', config.port);
+  console.info("Express listening on port", config.port);
 });
