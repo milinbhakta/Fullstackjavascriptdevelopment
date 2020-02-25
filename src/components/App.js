0acc604ad45cc16440f0c3ac7db504997a74eea5
Milind <desire2dosomething@gmail.com>
2020-02-24 22:30:52 -0500

    components life cycle

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 import React from "react";
 import Header from "./Header";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>...</div>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { pageHeader: 'Naming Contests' };
+
+  componentDidMount(){
+      console.log('Did Mount');
+      debugger;
+  }
+
+  componentWillUnmount(){
+      console.log('Will unmount');
+      debugger;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>...</div>
+      </div>
+    );
+  }
+}
 
 export default App;
